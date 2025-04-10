b2df6e3cc059135d93d0b27c5b6c5548d5d0be8e
Salman12-bit <msalman39707@gmail.com>
2025-04-10 12:57:02 +0500

    first

--- src/app/layout.js
+++ src/app/layout.js
@@ -10,6 +10,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+      <meta name="google-site-verification" content="SosA9MKmH1nQCnSZmleLMwQ530sXQTXTrTBK-p9ZAHU" />
       <link rel="icon" type="image/png" href="/favicon.ico" />
       </head>
       <body className={inter.className}>
